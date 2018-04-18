5bf6499b8ce319b697ce96f176ab74256e747ea7
DarrickAZ <zhwwhx@126.com>
2018-04-18 13:40:57 +0800

    ### update .

--- test03.py
+++ test03.py
@@ -1,1 +1,243 @@
-#高级特性
+###高级特性
+
+##切片
+# 取一个list或tuple的部分元素是非常常见的操作。比如，一个list如下：
+L = ['Michael', 'Sarah', 'Tracy', 'Bob', 'Jack']
+#取前3个元素
+#['Michael', 'Sarah', 'Tracy']
+print(L[0:3])
+#如果第一个索引是 0  ，还可以省略：
+#['Michael', 'Sarah', 'Tracy']
+print(L[:3])
+#既然Python支持 L[-1]  取倒数第一个元素，那么它同样支持倒数切片
+#['Bob', 'Jack']
+print(L[-2:])
+#['Bob']
+print(L[-2:-1])
+
+#构建0~99的数列
+S=list(range(100))
+#取出前10个数
+#[0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+print(S[:10])
+#取出后10个数
+#[90, 91, 92, 93, 94, 95, 96, 97, 98, 99]
+print(S[-10:])
+#前11~20个数
+#[10, 11, 12, 13, 14, 15, 16, 17, 18, 19]
+print(S[10:20])
+#前10个数，每2个取一个
+#[0, 2, 4, 6, 8]
+print(S[:10:2])
+#所有数，每5个取一个
+#[0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90, 95]
+print(S[::5])
+#什么都不写，S[:],即可复制一个list
+print(S[:])
+
+#tuple也是一种list,唯一的区别是tuple是不可变的，因此tuple也可以进行切片操作，只是切片后的结果，仍然是tuple
+tl=(1,2,3,4,5)
+#(1, 2, 3)
+print(tl[:3])
+# 字符串 'xxx'  也可以看成是一种list，每个元素就是一个字符。因此，字符串也可
+# 以用切片操作，只是操作结果仍是字符串：
+#'ABC'
+print('ABCDEFG'[:3])
+#'ACEG'
+print('ABCDEFG'[::2])
+
+##迭代
+# 如果给定一个list或tuple，我们可以通过 for  循环来遍历这个list或tuple，这种遍
+# 历我们称为迭代（Iteration）
+d={'a':1,'b':2,'c':3}
+#循环遍历key值
+for key in d:
+    print(key)
+#循环遍历vaule
+for value in d.values():
+    print(value)
+#同时迭代key和value
+for k,v in d.items():
+    print(k,v)
+#字符串也是可迭代对象
+for ch in 'ABC中国人abc':
+    print(ch)
+#判断所要迭代对象，是否为一个真正的可迭代对象
+from collections import Iterable
+#True
+print(isinstance('abc',Iterable))
+#True
+print(isinstance([1,2,3],Iterable))
+#False
+print(isinstance(123,Iterable))
+
+#如果要对list进行像java一样的处理 ，可选Python内置的enumerate函数
+# 0 A
+# 1 B
+# 2 C
+for i,value in enumerate(['A','B','C']):
+        print(i,value)
+# 1 1
+# 2 4
+# 5 9
+for x, y in [(1, 1), (2, 4), (5, 9)]:
+     print(x, y)
+
+##列表生成式
+# 列表生成式即List Comprehensions，是Python内置的非常简单却强大的可以用来
+# 创建list的生成式。
+#生成1~10的list序列
+li=list(range(1,11))
+[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+print(li)
+#如果要生成[1x1,2x2,3x3...10x10]怎么做？
+L=[]
+for x in range(1,11):
+    L.append(x*x)
+#[1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
+print(L)
+#简化上面循环
+#[1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
+print([x*x for x in range(1,11)])
+#[4, 16, 36, 64, 100]  筛选出偶数
+print([x * x for x in range(1, 11) if x % 2 == 0])
+#['AX', 'AY', 'AZ', 'BX', 'BY', 'BZ', 'CX', 'CY', 'CZ']  两次循环生成全排列
+print([m + n for m in 'ABC' for n in 'XYZ'])
+
+#三层和三层以上的循环就很少用到了。
+# 运用列表生成式，可以写出非常简洁的代码。例如，列出当前目录下的所有文件和
+# 目录名，可以通过一行代码实现：
+import os
+#列出目录下的所有文件
+#['AndroidWorkSpace', 'apache-maven-3.3.3', 'beimi', 'CWorkSpace', 'eclipse', 'eclipse-jee-oxygen-R-win32-x86_64.zip',
+# 'eclipse_EBusiness.lnk', 'GitRep', 'IDEAWorkSpace', 'JavaFXlogs', 'LazyLoad', 'Line01', 'Line02', 'NetBeansWorkSpace',
+# 'NodeJSWorkSpace', 'PythonWorkSpace', 'rpc', 'rpc - 副本', 'ScanLogin', 'SpringBoot-Learning', 'SpringBoot-Learning -
+# 副本 - 副本', 'springboot-learning-example - 副本', 'springcloud-learning-example - 副本', 'WeiXinApp', 'WorkSpace', 'WorkSpaceBlog',
+# '深入理解Java虚拟机：JVM高级特性与最佳实践（第2版）源代码', '深入理解Java虚拟机：JVM高级特性与最佳实践（第2版）源代码.zip']
+print([d for d in os.listdir('E:\\MyProject\\EBusiness')])
+
+#for  循环其实可以同时使用两个甚至多个变量，比如 dict  的 items()  可以同时迭代key和value：
+d={'x':'A','y':'B','z':'C'}
+# x A
+# y B
+# z C
+for k,v in d.items():
+    print(k,v)
+#列表生成式，也可以使用2个变量来生成list
+#['x=A', 'y=B', 'z=C']
+print([k+'=' +v for k,v in d.items()])
+#将所有字符串变为小写
+L = ['Hello', 'World', 'IBM', 'Apple']
+#['hello', 'world', 'ibm', 'apple']
+print([s.lower() for s in L])
+nL= ['Hello', 'World', 18, 'Apple', None]
+#['hello', 'world', 'apple']
+print([s.lower() for s in nL if isinstance(s,str)])
+
+##生成器
+# 通过列表生成式，我们可以直接创建一个列表。但是，受到内存限制，列表容量肯
+# 定是有限的。而且，创建一个包含100万个元素的列表，不仅占用很大的存储空
+# 间，如果我们仅仅需要访问前面几个元素，那后面绝大多数元素占用的空间都白白
+# 浪费了。
+# Python中，这种一边循环一边计算的机制，称为生成器：generator。
+# 要创建一个generator，有很多种方法。第一种方法很简单，只要把一个列表生成式
+# 的 []  改成 ()  ，就创建了一个generator：
+L=[x*x for x in range(10)]
+#[0, 1, 4, 9, 16, 25, 36, 49, 64, 81]
+print(L)
+g=(x*x for x in range(10))
+#0
+print(next(g))
+#1
+print(next(g))
+for n in g:
+    print(n)
+
+# 著名的斐波拉契数列（Fibonacci），除第一个和第二个数外，任意一个数都
+# 可由前两个数相加得到：
+# 1, 1, 2, 3, 5, 8, 13, 21, 34, ...
+# 斐波拉契数列用列表生成
+def fib(max):
+    n, a, b = 0, 0, 1
+    while n < max:
+         #yield b 变成generator
+         print(b)
+         a, b = b, a + b
+         n = n + 1
+    return 'done'
+
+#
+print(fib(6))
+
+def odd():
+    print('step 1')
+    yield 1
+    print('step 2')
+    yield(3)
+    print('step 3')
+    yield(5)
+# 调用该generator时，首先要生成一个generator对象，然后用 next()  函数不断获
+# 得下一个返回值：
+#1
+# print(next(odd()))
+# print(next(odd()))
+# print(next(odd()))
+
+o=odd()
+print(next(o))
+print(next(o))
+print(next(o))
+#StopIteration
+#print(next(o))
+
+## 迭代器
+# 我们已经知道，可以直接作用于 for  循环的数据类型有以下几种：
+# 一类是集合数据类型，如 list  、 tuple  、 dict  、 set  、 str  等；
+# 一类是 generator  ，包括生成器和带 yield  的generator function。
+# 这些可以直接作用于 for  循环的对象统称为可迭代对象： Iterable  。
+# 可以使用 isinstance()  判断一个对象是否是 Iterable  对象：
+from collections import Iterable
+#True
+print(isinstance([],Iterable))
+#True
+print(isinstance((),Iterable))
+#可以使用 isinstance()  判断一个对象是否是 Iterator  对象 next()函数
+from collections import Iterator
+#False
+print(isinstance([],Iterator))
+#True
+print(isinstance((x for x in range(10)),Iterator))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- test04.py
+++ test04.py
@@ -1,5 +1,123 @@
+###函数式编程
+# 函数式编程的一个特点就是，允许把函数本身作为参数传入另一个函数，还允许返
+# 回一个函数！
+# Python对函数式编程提供部分支持。由于Python允许使用变量，因此，Python不是
+# 纯函数式编程语言。
 
+##高阶函数
+# 变量可以指向函数
+# 以Python内置的求绝对值的函数 abs()  为例，调用该函数用以下代码：
+#10
+print(abs(-10))
+#变量可以指向函数
+#<built-in function abs>
+#print(abs)
+x=abs(-10)
+#10
+print(x)
+#将函数赋值给变量
+f=abs
+#10
+print(f(-10))
 
+##传入函数
+# 既然变量可以指向函数，函数的参数能接收变量，那么一个函数就可以接收另一个
+# 函数作为参数，这种函数就称之为高阶函数
+#一个最简单的高阶函数
+def add(x,y,f):
+    return f(x)+f(y)
+#10
+print(add(-1,9,abs))
+#x = -1
+#y = 9
+#f = abs
+#f(x) + f(y) ==> abs(-1) + abs(9) ==> 10
+#return 10
+
+#map/reduce Python内建了 map()  和 reduce()  函数。
+#map()  函数接收两个参数，一个是函数，一个
+# 是 Iterable  ， map  将传入的函数依次作用到序列的每个元素，并把结果作为新
+# 的 Iterator  返回。
+def f(x):
+    return x*x
+r=map(f,[1,2,3,4,5,6])
+# map()  传入的第一个参数是 f  ，即函数对象本身。由于结果 r  是一
+# 个 Iterator  ， Iterator  是惰性序列，因此通过 list()  函数让它把整个序列
+# 都计算出来并返回一个list。
+#[1, 4, 9, 16, 25, 36]
+print(list(r))
+#['1', '2', '3', '4']
+print(list(map(str,[1,2,3,4])))
+
+# reduce  把一个函数作用在一个序列 [x1, x2, x3,
+# ...]  上，这个函数必须接收两个参数， reduce  把结果继续和序列的下一个元素
+# 做累积计算，其效果就是：
+#序列求和
+from functools import reduce
+def add(x,y):
+    return x+y
+#16
+print(reduce(add,[1,3,5,7]))
+
+def fn(x, y):
+     return x * 10 + y
+#13579
+print(reduce(fn, [1, 3, 5, 7, 9]))
+# 1 3
+#1*10 + 3
+#13*10 + 5
+# 135*10 +7
+# 1357*10 +9
+#13579
+
+#将str转为int的函数
+# def char2num(s):
+#     return {'0':0,'1':1,'2':2,'3':3,'4':4,'5':5,'6':6,'7':7,'8':8,'9':9}
+# #
+# print(reduce(fn,map(char2num ,'13579')))
+
+##filer
+# Python内建的 filter()  函数用于过滤序列。
+# 和 map()  类似， filter()  也接收一个函数和一个序列。和 map()  不同的
+# 时， filter()  把传入的函数依次作用于每个元素，然后根据返回值是 True  还
+# 是 False  决定保留还是丢弃该元素。
+# 例如，在一个list中，删掉偶数，只保留奇数，可以这么写：
+def is_odd(n):
+    return n % 2 == 1
+#[1, 5, 9, 15]
+print(list(filter(is_odd, [1, 2, 4, 5, 6, 9, 10, 15])))
+
+# 把一个序列中的空字符串删掉，可以这么写：
+def not_empty(s):
+    return s and s.strip()
+#['A', 'B', 'C']
+print(list(filter(not_empty,['A','','B',None,'C',' '])))
+
+#定义一个生成器，生成无限序列
+def _odd_iter():
+    n=1
+    while True:
+        n=n+2
+        yield n
+#定义一个筛选函数
+def _not_divisible(n):
+    return lambda x:x%n>0
+
+#定义一个生成器，不断返回下一个素数
+def primes():
+    yield 2
+    it = _odd_iter() # 初始序列
+    while True:
+        n = next(it) # 返回序列的第一个数
+        yield n
+        it = filter(_not_divisible(n), it) # 构造新序列
+
+# 打印1000以内的素数:
+for n in primes():
+    if n < 10:
+        print(n)
+    else:
+        break
 
 
 
